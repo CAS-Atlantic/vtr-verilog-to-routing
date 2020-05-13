faf45f4311c17f9696e7d4054778ed546be2bacd
emacdo12 <emacdo12@unb.ca>
2020-05-13 08:26:32 -0300

    added input, took out inout because it's not supported

--- ODIN_II/regression_test/benchmark/verilog/Keywords/generate.v
+++ ODIN_II/regression_test/benchmark/verilog/Keywords/generate.v
@@ -1,30 +1,30 @@
-module test(out);
 
-  reg       [7:0] i = 8'b10101010;
-  wire      [7:0] o;
-  output    [7:0] out;
+`define WIDTH 8
 
-  // Generate Block, Verilog-2001
-  genvar index;
-  generate
-    for (index = 0; index < 8;
-      index = index + 1) begin
-      buffer_not NBUF(
-        .in(i[index]),
-        .out(o[index]));
+module simple_op(a,b,out);
+    input   [`WIDTH-1:0]a;
+    input   [`WIDTH-1:0]b;
+    output  [`WIDTH-1:0]out;
+    wire    [`WIDTH-1:0]o;
+
+genvar i;
+generate
+    for (i = 0; i < `WIDTH;i = i + 1) begin
+      nand_node NNAND(
+        .in(a[i]),.inn(b[i]),
+        .out(o[i]));
     end
   endgenerate
   assign out = o;
 endmodule
 
-module buffer_not(in, out);
+module nand_node(in,inn,out);
   input in;
+  input inn;
   output out;
+  wire temp;
   
-  assign out = ~in;
-
-endmodule
-
-//example taken from playground 
-//error Error: Could not hook up the pin test^BUF~16: not available.
-//not producing the input vectors at all and finding error
+  nand(temp,in,inn);
+  assign out = temp;
+  
+endmodule 
