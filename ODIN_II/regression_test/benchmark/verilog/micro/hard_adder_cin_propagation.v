ba3a2b5d27297760aba4b65cf0116765c6680d14
s.alireza.damghani@unb.ca <s.alireza.damghani@unb.ca>
2020-05-31 01:31:35 -0300

    covering all possible combinations of inputs

--- ODIN_II/regression_test/benchmark/verilog/micro/hard_adder_cin_propagation.v
+++ ODIN_II/regression_test/benchmark/verilog/micro/hard_adder_cin_propagation.v
@@ -1,13 +1,13 @@
 module top_module
 (
-    input  [2:0] a, b, c,
+    input  [1:0] a, b, c,
     input cin,
-    output [2:0] sumout,
+    output [1:0] sumout,
     output cout
 );
 
 	wire  cout1;
-	wire [2:0] sumout1;	
+	wire [1:0] sumout1;	
 
 	adder add1 (a, b, cin, sumout1, cout1);
 	adder add2 (.a(c), .b(sumout1), .cin(cout1), .sumout(sumout), .cout(cout));
