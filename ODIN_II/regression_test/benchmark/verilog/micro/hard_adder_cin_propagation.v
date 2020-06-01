d0202366c9b593b8fdc264dc7f24fe13582c64a1
s.alireza.damghani@unb.ca <s.alireza.damghani@unb.ca>
2020-06-01 10:52:00 -0300

    change the structure of a benchmark

--- ODIN_II/regression_test/benchmark/verilog/micro/hard_adder_cin_propagation.v
+++ ODIN_II/regression_test/benchmark/verilog/micro/hard_adder_cin_propagation.v
@@ -1,15 +1,12 @@
 module top_module
 (
-    input  [1:0] a, b, c,
-    input cin,
-    output [1:0] sumout,
-    output cout
+    input  [3:0] a, b,
+    output [4:0] out
 );
 
-	wire  cout1;
-	wire [1:0] sumout1;	
+	wire  cout;
 
-	adder add1 (a, b, cin, sumout1, cout1);
-	adder add2 (.a(c), .b(sumout1), .cin(cout1), .sumout(sumout), .cout(cout));
+	adder add1(.a(a[1:0]), .b(b[1:0]), .cin(1'b0), .sumout(out[1:0]), .cout(cout));
+	adder add2(.a(a[3:2]), .b(b[3:2]), .cin(cout), .sumout(out[3:2]), .cout(out[4]));
 
 endmodule
