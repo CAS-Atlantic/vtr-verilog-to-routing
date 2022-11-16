54c99b79417fac48ee9553e45b6aabd31d0d844a
root <al.azadi94@unb.ca>
2022-11-16 18:05:12 -0400

    [Yosys + Odin] Updated Yosys Frontend
    Added support for new yosys memories mem_v2 as YMEM2
    
    Old Yosys replaced with newest version 0.23
    
    -norom will omit the proc_rom pass
    
    Odin techlib support for aldff and aldffe added
    
    test results regenerated
    
    Co-authored-by: Alireza Azadi <al.azadi94@unb.ca>
    Co-authored-by: Daniel Khadivi <d.khadivi@unb.ca>

--- vtr_flow/misc/yosyslib/synthesis.tcl
+++ vtr_flow/misc/yosyslib/synthesis.tcl
@@ -69,6 +69,8 @@ opt -full
 # Transform all async FFs into synchronous ones
 techmap -map +/adff2dff.v
 techmap -map TTT/../../../ODIN_II/techlib/adffe2dff.v
+techmap -map TTT/../../../ODIN_II/techlib/aldff2dff.v
+techmap -map TTT/../../../ODIN_II/techlib/aldffe2dff.v
 
 # Map multipliers, DSPs, and add/subtracts according to yosys_models.v
 techmap -map YYY */t:\$mul */t:\$mem */t:\$sub */t:\$add
